ef711c0de1fdf50d41fb89da41c8ed6f2e25ffd8
paulsebeikin <paulsebeikin@hotmail.com>
2016-05-13 13:51:22 +0200

    Login and About Screen Work

---
+++ dev/MAPme/app/src/main/java/psyblaze/mapme/AboutActivity.java
@@ -0,0 +1,41 @@
+package psyblaze.mapme;
+
+import android.content.Intent;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar action_bar = (Toolbar) findViewById(R.id.mapme_toolbar);
+        setSupportActionBar(action_bar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        //return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                Toast.makeText(this,"Logging out...", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
