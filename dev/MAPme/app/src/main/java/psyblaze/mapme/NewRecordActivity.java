329289df8bfa2efc19c54059cbdd79e0e2c14a23
paulsebeikin <paulsebeikin@hotmail.com>
2016-05-16 13:36:24 +0200

    NewRecord Screen #2
    Screen 1 - added drop down, date picker, various buttons.

--- dev/MAPme/app/src/main/java/psyblaze/mapme/NewRecordActivity.java
+++ dev/MAPme/app/src/main/java/psyblaze/mapme/NewRecordActivity.java
@@ -1,28 +1,27 @@
 package psyblaze.mapme;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ExpandableListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleExpandableListAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NewRecordActivity extends AppCompatActivity {
 
-    ListView listView;
     Spinner proj_spinner;
+    TextView datePicker;
+    SelectDateFragment datePickerFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +30,24 @@ public class NewRecordActivity extends AppCompatActivity {
         Toolbar action_bar = (Toolbar) findViewById(R.id.mapme_toolbar);
         setSupportActionBar(action_bar);
 
+        String[] values = getResources().getStringArray(R.array.Projects);
+
+        // get datePicker element and set default date as today
+        datePicker = (TextView) findViewById(R.id.date_picker);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        datePicker.setText(sdf.format(new Date()));
+
+        // fetch spinner object from the layout
         proj_spinner = (Spinner)findViewById(R.id.project_spinner);
+
+        // create adapter for spinner
+        ArrayAdapter spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, values);
+
+        // configure drop-down layout style
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        // attaching data adapter to spinner
+        proj_spinner.setAdapter(spinnerAdapter);
     }
 
     @Override
@@ -59,4 +75,23 @@ public class NewRecordActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void showDatePickerDialog(View view){
+        datePickerFragment = new SelectDateFragment();
+        Bundle args = new Bundle();
+        datePickerFragment.setArguments(args);
+
+        datePickerFragment.setOnDateSetListener(new DatePickerDialog.OnDateSetListener(){
+            public void onDateSet(DatePicker view, int year, int month, int day){
+                datePicker.setText(day + "/" + month + "/" + year);
+            }
+        });
+        datePickerFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void navigateNext(View view){
+        Intent nextInt = new Intent(this, NewRecordActivity2.class);
+        startActivity(nextInt);
+    }
+
 }
---
+++ dev/MAPme/app/src/main/java/psyblaze/mapme/NewRecordActivity2.java
@@ -0,0 +1,49 @@
+package psyblaze.mapme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class NewRecordActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record2);
+
+        Toolbar action_bar = (Toolbar) findViewById(R.id.mapme_toolbar);
+        setSupportActionBar(action_bar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbar_home, menu);
+        //return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                Intent homeInt = new Intent(this, HomeScreenActivity.class);
+                startActivity(homeInt);
+                return true;
+            case R.id.action_logout:
+                Toast.makeText(this,"Logging out...", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_about:
+                Intent aboutInt = new Intent(this, AboutActivity.class);
+                startActivity(aboutInt);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
