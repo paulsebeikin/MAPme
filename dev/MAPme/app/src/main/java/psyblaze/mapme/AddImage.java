032bd1691dfefd5c444da7330fff332682d648f1
dylsmith8 <dylan.smith888@gmail.com>
2016-05-23 07:40:16 +0200

    Initial Commit

--- dev/MAPme/app/src/main/java/psyblaze/mapme/AddImage.java
+++ dev/MAPme/app/src/main/java/psyblaze/mapme/AddImage.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.content.res.Resources;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
