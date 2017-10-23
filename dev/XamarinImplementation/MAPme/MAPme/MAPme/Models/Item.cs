8ba0415625a335273955a34143ccfe689983c873
Dylan Smith <dylan.smith888@gmail.com>
2017-10-23 22:01:28 +0200

    Included Sqlite-net from source with some test classes.

--- dev/XamarinImplementation/MAPme/MAPme/MAPme/Models/Item.cs
+++ dev/XamarinImplementation/MAPme/MAPme/MAPme/Models/Item.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DAL.SQLite;
 
 namespace MAPme
 {
@@ -7,5 +8,22 @@ namespace MAPme
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+    }
+
+    public class Stock
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+    }
+
+    public class Valuation
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public int StockId { get; set; }
+        public DateTime Time { get; set; }
+        public decimal Price { get; set; }
     }
 }
