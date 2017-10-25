090dab7bbaee7a04178ee23bfa27125b02e03042
Dylan Smith <dylan.smith888@gmail.com>
2017-10-25 21:17:33 +0200

    ADU URLs and API keys

---
+++ dev/XamarinImplementation/MAPme/MAPme/MAPme/DAL/AduApiKeys.cs
@@ -0,0 +1,53 @@
+﻿using DAL.SQLite;
+
+namespace MAPme.DAL
+{
+    // ported from Web.java
+    public class AduApiKeys
+    {
+        [NotNull]
+        public string Token { get; private set; }
+        [NotNull]
+        public string ApiKey { get; private set; }
+        [NotNull]
+        public string ApiKeyAlt { get; private set; }
+        [NotNull]
+        public string PassId { get; private set; }
+        [NotNull]
+        public string Username { get; private set; }
+        [NotNull]
+        public string UserId { get; private set; }
+        [NotNull]
+        public string Email { get; private set; }
+        [NotNull]
+        public string Project { get; private set; }
+        [NotNull]
+        public string Latitude { get; private set; }
+        [NotNull]
+        public string Longitude { get; private set; }
+        [NotNull]
+        public string MinimumElevation { get; private set; }
+        [NotNull]
+        public string MaximumElevation { get; private set; }
+        [NotNull]
+        public string Country { get; private set; }
+        [NotNull]
+        public string Province { get; private set; }
+        [NotNull]
+        public string NearestTown { get; private set; }
+        [NotNull]
+        public string Locality { get; private set; }
+        [NotNull]
+        public string Day { get; private set; }
+        [NotNull]
+        public string Month { get; private set; }
+        [NotNull]
+        public string Year { get; private set; }
+        [NotNull]
+        public string Source { get; private set; }
+        [NotNull]
+        public string Images { get; private set; }
+        [NotNull]
+        public string Note { get; private set; }
+    }
+}
---
+++ dev/XamarinImplementation/MAPme/MAPme/MAPme/DAL/AduSiteData.cs
@@ -0,0 +1,19 @@
+﻿using DAL.SQLite;
+
+namespace MAPme.DAL
+{
+    public class AduSiteData
+    {
+        // ported from constants in Web.java
+        [NotNull]
+        public string AuthenticationUrl { get; private set; }
+        [NotNull]
+        public string ValidationUrl { get; private set; }
+        [NotNull]
+        public string TestUrl { get; private set; }
+        [NotNull]
+        public string PostUrl { get; private set; }
+        [NotNull]
+        public string ProjectUrl { get; private set; }
+    }
+}
