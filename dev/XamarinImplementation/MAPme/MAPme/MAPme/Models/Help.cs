ca5d5c9951f328042e893b6e3fe18f2e84fe7738
Dylan Smith <dylan.smith888@gmail.com>
2017-10-26 17:55:57 +0200

    Table for help literals

---
+++ dev/XamarinImplementation/MAPme/MAPme/MAPme/Models/Help.cs
@@ -0,0 +1,20 @@
+﻿using DAL.SQLite;
+
+namespace MAPme.Models
+{
+    // ported from HelpLiterals.java
+    // scripted in ?
+    public class Help
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; private set; }
+        [NotNull]
+        public string NewRecordHelp { get; private set; }
+        [NotNull]
+        public string LoginHelp { get; private set; }
+        [NotNull]
+        public string ProfileHelp { get; private set; }
+        [NotNull]
+        public string HistoryHelp { get; private set; }
+    }
+}
