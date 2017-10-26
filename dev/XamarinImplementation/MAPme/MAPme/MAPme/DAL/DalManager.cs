e3c3d9e03ecd63c0e4e48552ffe9d3c77908b909
Dylan Smith <dylan.smith888@gmail.com>
2017-10-26 18:44:26 +0200

    DAL Manager (WIP)

--- dev/XamarinImplementation/MAPme/MAPme/MAPme/DAL/DalManager.cs
+++ dev/XamarinImplementation/MAPme/MAPme/MAPme/DAL/DalManager.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using MAPme.DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using DAL.SQLite;
 
 namespace MAPme.DAL
 {
-    class DalManager
+    public class DalManager : IDalInterface
     {
+        public void CreateTable<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(SQLiteConnection db, T table, string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDatabasePath(string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SQLiteConnection GetSQLiteConnection(string databasePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert<T>(T table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Query<T>(SQLiteConnection db, T table, string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T>(SQLiteConnection db, T table, string sql)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+S
---
+++ dev/XamarinImplementation/MAPme/MAPme/MAPme/DAL/Interfaces/IDalInterface.cs
@@ -0,0 +1,19 @@
+﻿using DAL.SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAPme.DAL.Interfaces
+{
+    public interface IDalInterface
+    {
+        string GetDatabasePath(string location);
+        SQLiteConnection GetSQLiteConnection(string databasePath);  // synchronous DB connection
+
+        void CreateTable<T>();
+        void Insert<T>(T table);
+        IEnumerable<T> Query <T>(SQLiteConnection db, T table, string sql);
+        void Update<T>(SQLiteConnection db, T table, string sql);
+        void Delete<T>(SQLiteConnection db, T table, string sql);
+    }
+}
